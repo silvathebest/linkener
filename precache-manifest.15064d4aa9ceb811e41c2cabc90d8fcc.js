f65927f8fdfd6b5a80ffd1de2ea030947986e824
silvathebest <silverchat228@gmail.com>
2020-07-13 17:25:19 +0400

    Updates

---
+++ precache-manifest.15064d4aa9ceb811e41c2cabc90d8fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78cc725563097c304093118f39ffc68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93bd5e7feb5b98706e41",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "c62d9b0cf052ea011d87",
+    "url": "/static/css/main.006bf8c7.chunk.css"
+  },
+  {
+    "revision": "93bd5e7feb5b98706e41",
+    "url": "/static/js/2.3df430fd.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.3df430fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62d9b0cf052ea011d87",
+    "url": "/static/js/main.68764789.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
